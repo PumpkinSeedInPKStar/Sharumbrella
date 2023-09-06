b9b8be15c2df0516d045bec1b9b04c3778353e51
rnjswlals1028 <112091074+rnjswlals1028@users.noreply.github.com>
2023-09-06 19:52:06 +0900

    new

--- app/src/main/java/com/nanumy/sharumbrella/FindID_phone.java
+++ app/src/main/java/com/nanumy/sharumbrella/FindID_phone.java
@@ -1,14 +1,36 @@
 package com.nanumy.sharumbrella;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class FindID_phone extends AppCompatActivity {
+
+    private FirebaseAuth mFirebaseAuth;
+    private DatabaseReference mDatabaseRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_id_phone);
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mDatabaseRef = FirebaseDatabase.getInstance().getReference("Sharumbrella");
+
+        mBtnBackground = findViewById(R.id.background);
     }
 }
